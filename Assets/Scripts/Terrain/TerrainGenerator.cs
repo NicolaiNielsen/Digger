72359fab493cc9681b4dae71389dff590a1871ce
NicolaiNielsen <Nicolaiaphichat@gmail.com>
2025-05-08 05:45:58 +0200

    gizmosdrawn

--- Assets/Scripts/Terrain/TerrainGenerator.cs
+++ Assets/Scripts/Terrain/TerrainGenerator.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class TerrainGenerator : MonoBehaviour
 {
     [SerializeField] int xSize = 10;
-    [SerializeField] int ySize = 10;
+    [SerializeField] int zSize = 10;
 
     private Mesh mesh;
+    Vector3[] vertices;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,6 +23,32 @@ public class TerrainGenerator : MonoBehaviour
     }
 
     private void GenerateTerrain() {
-        Debug.Log("TEst");
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        int i = 0;
+
+        for(int z = 0; z <= zSize; z++) {
+            for(int x = 0; x <= xSize; x++) {
+                vertices[i] = new Vector3(x, 0 , z);
+                i++;
+
+            }
+        }
+
+        mesh.vertices = vertices;
+
     }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+            return;
+
+        Gizmos.color = Color.red;
+        foreach (Vector3 pos in vertices)
+        {
+            Gizmos.DrawSphere(pos, 0.2f);
+        }
+    }
+
 }
